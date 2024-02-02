7ceb05cc7bc649f59574f75997f3bf481cf12aa8
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-02-02 15:18:49 +0100

    Start implement backend features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+'use client';
+import { User } from "@prisma/client";
+import { createUser, getUser } from "./actions/user";
+
+
 export default function Home() {
+
+  async function getUser2(){
+    const u = await getUser();
+    
+    console.log(u)
+  }
+
   return (
     <main>
+      <button onClick={() => createUser()}>
+        createuser
+      </button>
       <h1>Social Sparkz</h1>
+      <button onClick={() => (getUser2())}>testuser</button>
     </main>
   );
 }
