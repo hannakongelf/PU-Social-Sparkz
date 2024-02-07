1c0d15d2f0185a40ffde7b95d45b1baeb29a99e9
simonbjerkas <simon.bjerkas10@gmail.com>
2024-02-07 16:10:01 +0100

    refactor: added proper login an logout buttons in header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-'use client';
-
 import Navbar from '@/components/Navbar';
-import { useSession } from 'next-auth/react';
 
 export default function Home() {
-  const session = useSession();
   return (
     <main>
       <header>
         <Navbar />
       </header>
       <h1>Social Sparkz</h1>
-      {session.data?.user && <div>Velkommen {session.data.user.name}</div>}
     </main>
   );
 }
