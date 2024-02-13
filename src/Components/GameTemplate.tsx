7537d05d8e7413bbba36654bbb1d022cc2a2f23c
hannakongelf <122216425+hannakongelf@users.noreply.github.com>
2024-02-13 14:37:12 +0100

    feat: added <required> attribute to required input fields, created button for submitting form, created header and paragraph.
    Co-authored-by: irilstr <irilstr@users.noreply.github.com>

--- src/Components/GameTemplate.tsx
+++ src/Components/GameTemplate.tsx
@@ -1,23 +1,24 @@
 "use client";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { Game } from "@prisma/client";
 import { useRouter } from "next/navigation";
+
 
 export default function GameTemplate() {
     const router = useRouter();
 
     return (
-        <form className="">
+        <form id="newGameForm" name="newGameForm" className="">
             
+            <h1>
+                Make a new game!
+            </h1>
+            <p>
+                Please fill out the fields below. Please note that some of the fields are required, while some are optional.
+            </p>
+
             <div>
                 <label htmlFor="gname">Game name:</label><br></br>
-                <input type="text" id="gname" name="gname"></input><br></br>
+                <input type="text" id="gname" name="gname" required></input><br></br>
             </div>
 
             <div>
@@ -27,7 +28,7 @@ export default function GameTemplate() {
 
             <div>
                 <label htmlFor="desc">Game description:</label><br></br>
-                <textarea id="desc" name="desc" rows={4} cols={50}></textarea><br></br>
+                <textarea id="desc" name="desc" rows={4} cols={50} required></textarea><br></br>
             </div>
 
             <div>
@@ -37,20 +38,23 @@ export default function GameTemplate() {
 
             <div>
                 <label htmlFor="minplayers">Minimum number of players:</label><br></br>
-                <input type="number" id="minplayers" name="minplayers" value={1}></input><br></br>
+                <input type="number" id="minplayers" name="minplayers" value={1} required></input><br></br>
                 <label htmlFor="maxplayers">Maximum number of players:</label><br></br>
-                <input type="number" id="maxplayers" name="maxplayers"></input><br></br>
+                <input type="number" id="maxplayers" name="maxplayers" required></input><br></br>
             </div>
 
             <div>
                 <label htmlFor="gcategory">Choose a suitable category:</label><br></br>
-                <select id="gcategory" name="gcategory">
+                <select id="gcategory" name="gcategory" required>
                     <option value="music">Music</option>
                     <option value="pregame">Pregame</option>
                     <option value="boardgames">Boardgames</option>
                 </select><br></br>
             </div>
 
+            <div>
+                <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300">Create game</button>
+            </div>
         
         </form>
     )
