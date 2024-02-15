3431cad545f141c4b8ab5d27b1943d5b1d018e48
simonbjerkas <simon.bjerkas10@gmail.com>
2024-02-15 14:34:38 +0100

    Removed: merge markers

--- src/components/list-content.tsx
+++ src/components/list-content.tsx
@@ -1,22 +1,22 @@
-"use client";
-import { useState, useEffect } from "react";
-import ListCard from "@/components/list-card";
-import Grid from "@/components/common/grid";
-import { Button, TextField } from "@mui/material";
-import { GameWithReviews } from "@/db/queries/game";
-import { gameType } from "@prisma/client";
+'use client';
+import { useState, useEffect } from 'react';
+import ListCard from '@/components/list-card';
+import Grid from '@/components/common/grid';
+import { Button, TextField } from '@mui/material';
+import { GameWithReviews } from '@/db/queries/game';
+import { gameType } from '@prisma/client';
 
 export default function ListContent({ games }: { games: GameWithReviews[] }) {
   const [filteredGames, setFilteredGames] = useState<GameWithReviews[]>(games);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [category, setCategory] = useState<gameType | null>(null);
   const [sortKey, setSortKey] = useState<keyof GameWithReviews | null>(null);
-  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
 
   useEffect(() => {
     let tempFilteredGames = games.filter((game) => {
       const matchesSearchTerm =
-        searchTerm === "" ||
+        searchTerm === '' ||
         game.name.toLowerCase().includes(searchTerm.toLowerCase());
       const matchesCategory = category === null || game.type === category;
       return matchesSearchTerm && matchesCategory;
@@ -27,12 +27,12 @@ export default function ListContent({ games }: { games: GameWithReviews[] }) {
         let valueA = a[sortKey];
         let valueB = b[sortKey];
 
-        if (typeof valueA === "string" && typeof valueB === "string") {
+        if (typeof valueA === 'string' && typeof valueB === 'string') {
           valueA = valueA.toLowerCase();
           valueB = valueB.toLowerCase();
         }
 
-        if (sortDirection === "asc") {
+        if (sortDirection === 'asc') {
           return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
         } else {
           return valueA > valueB ? -1 : valueA < valueB ? 1 : 0;
@@ -43,38 +43,32 @@ export default function ListContent({ games }: { games: GameWithReviews[] }) {
     setFilteredGames(tempFilteredGames);
   }, [searchTerm, category, games, sortKey, sortDirection]);
 
-<<<<<<< HEAD
-=======
-    filterGames();
-  }, [searchTerm, category, games]);
-
->>>>>>> 7c73b22 (Create detail view for game with title, description, rating, and)
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
   const handleSortChange = (key: keyof GameWithReviews) => {
     setSortKey(key);
-    setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
   };
 
   return (
     <div>
-      <div className="my-6 flex justify-center">
+      <div className='my-6 flex justify-center'>
         <TextField
-          id="outlined-basic"
-          label="Søk"
-          variant="outlined"
+          id='outlined-basic'
+          label='Søk'
+          variant='outlined'
           value={searchTerm}
           onChange={handleSearchChange}
         />
       </div>
 
-      <Button onClick={() => handleSortChange("name")}>
+      <Button onClick={() => handleSortChange('name')}>
         Sort by Name {sortDirection}
       </Button>
 
-      <div className="flex justify-center my-6 flex-wrap gap-2">
+      <div className='flex justify-center my-6 flex-wrap gap-2'>
         {Object.values(gameType).map((t) => (
           <Button
             key={t}
@@ -94,7 +88,7 @@ export default function ListContent({ games }: { games: GameWithReviews[] }) {
         </Button>
       </div>
 
-      <Grid className="grid-cols-5 gap-5">
+      <Grid className='grid-cols-5 gap-5'>
         {filteredGames.map((g) => (
           <ListCard game={g} key={g.id} />
         ))}
