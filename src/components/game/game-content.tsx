59be869bb0353a0f03545c5403cba21c921b9802
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-02-21 12:44:02 +0100

    Add default fallback image and use image on detail page. Fix bug where reviews go to game id = 1 automatically

--- src/components/game/game-content.tsx
+++ src/components/game/game-content.tsx
@@ -1,30 +1,32 @@
-'use client';
+"use client";
 
-import { GameWithReviews } from '@/db/queries/game';
-import Image from 'next/image';
-import { Rating } from '@mui/material';
-import ReviewContent from './review-content';
-import RatingCard from '@/components/game/rating-card';
+import { GameWithReviews } from "@/db/queries/game";
+import Image from "next/image";
+import { Rating } from "@mui/material";
+import ReviewContent from "./review-content";
+import RatingCard from "@/components/game/rating-card";
 
 const GameContent = ({ game }: { game: GameWithReviews }) => {
   return (
     <>
-      <div className='flex gap-2'>
-        <section className='bg-[#845EC2] text-white shadow-2xl rounded'>
-          <h1 className='text-center p-4'>{game.type}</h1>
+      <div className="flex gap-2">
+        <section className="bg-[#845EC2] text-white shadow-2xl rounded">
+          <h1 className="text-center p-4">{game.type}</h1>
           <Image
             src={
-              'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'
+              game.image && game.image?.length > 0
+                ? game.image
+                : "/stock_game.jpg"
             }
-            alt='Sample Image'
+            alt="Sample Image"
             width={900}
             height={300}
-            objectFit='cover'
+            objectFit="cover"
           />
-          <div className='flex gap-2 p-2'>
+          <div className="flex gap-2 p-2">
             <p>Brukeres vurdering av leken</p>
             <Rating
-              name='read-only'
+              name="read-only"
               value={
                 game.Review.map((r) => r.rating).reduce((a, b) => a + b, 0) /
                 game.Review.filter((r) => !!r.rating).length
@@ -36,7 +38,7 @@ const GameContent = ({ game }: { game: GameWithReviews }) => {
         <RatingCard game={game.id} />
       </div>
 
-      <h2 className='text-4xl'>{game.name}</h2>
+      <h2 className="text-4xl">{game.name}</h2>
       <p>{game.description}</p>
 
       {game.Review.map((review) => (
