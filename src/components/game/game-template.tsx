f5e7d5e958ed546c498caa2b56e46dd6fb7df796
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-02-22 09:49:45 +0100

    Fix various bugs
    Fix stuff

--- src/components/game/game-template.tsx
+++ src/components/game/game-template.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import Input from '../common/input';
+import * as React from "react";
+import Input from "../common/input";
 
-import { useFormState } from 'react-dom';
-import * as actions from '@/actions';
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
 import {
   Button,
   FormControl,
@@ -13,118 +13,120 @@ import {
   Paper,
   Select,
   TextField,
-} from '@mui/material';
+} from "@mui/material";
 
 export default function GameTemplate() {
   const [formState, action] = useFormState(actions.createGame, {
     errors: {},
   });
 
-  const [selectedCategory, setSelectedCategory] = React.useState('');
+  const [selectedCategory, setSelectedCategory] = React.useState("");
 
   const handleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedCategory(event.target.value);
   };
   return (
-    <div className='flex-col justify-center mt-10'>
-      <div className='flex justify-center pt-8 pb-8 bg-[#845EC2] bg-im'>
-        <h1 className='text-4xl flex justify-center text-white'>
+    <div className="flex-col justify-center mt-10">
+      <div className="flex justify-center pt-8 pb-8 bg-[#845EC2] bg-im">
+        <h1 className="text-4xl flex justify-center text-white">
           Create New Game
         </h1>
       </div>
-      <Paper elevation={3} className='flex justify-center mb-5 pb-10'>
-        <form action={action} className='w-4/5'>
-          <p className='mb-4 mt-5 text-center'>
+      <Paper elevation={3} className="flex justify-center mb-5 pb-10">
+        <form action={action} className="w-4/5">
+          <p className="mb-4 mt-5 text-center">
             Please fill out the fields below. Please note that some fields are
             required, while others are optional.
           </p>
 
-          <div className='flex flex-col mb-3'>
-            <label htmlFor='name'>
-              Game name: <span className='text-red-500'>*</span>
+          <div className="flex flex-col mb-3">
+            <label htmlFor="name">
+              Game name: <span className="text-red-500">*</span>
             </label>
-            <Input name='name' required error={!!formState.errors.name}></Input>
-            <p className='bg-red-500 text-white'>{formState.errors.name}</p>
+            <Input name="name" required error={!!formState.errors.name}></Input>
+            <p className="bg-red-500 text-white">{formState.errors.name}</p>
           </div>
 
-          <div className='flex flex-col mb-3'>
-            <label htmlFor='pic'>Upload a picture-url:</label>
-            <Input name='pic'></Input>
+          <div className="flex flex-col mb-3">
+            <label htmlFor="pic">Upload a picture-url:</label>
+            <Input name="pic"></Input>
           </div>
 
-          <div className='flex flex-col mb-3'>
-            <label htmlFor='description'>
-              Game description: <span className='text-red-500'>*</span>
+          <div className="flex flex-col mb-3">
+            <label htmlFor="description">
+              Game description: <span className="text-red-500">*</span>
             </label>
             <TextField
-              id='desc'
-              name='description'
+              id="desc"
+              name="description"
               required
               multiline
               minRows={3}
             />
           </div>
 
-          <div className='flex flex-col mb-3'>
-            <label htmlFor='minplayers'>
-              Minimum number of players: <span className='text-red-500'>*</span>
+          <div className="flex flex-col mb-3">
+            <label htmlFor="minplayers">
+              Minimum number of players: <span className="text-red-500">*</span>
             </label>
             <Input
-              type='number'
-              name='minplayers'
+              type="number"
+              name="minplayers"
               required
+              min={2}
               error={!!formState.errors.playerMin}
             ></Input>
-            <p className='bg-red-500 text-white'>
+            <p className="bg-red-500 text-white">
               {formState.errors.playerMin}
             </p>
           </div>
 
-          <div className='flex flex-col mb-3'>
-            <label htmlFor='maxplayers'>
-              Maximum number of players: <span className='text-red-500'>*</span>
+          <div className="flex flex-col mb-3">
+            <label htmlFor="maxplayers">
+              Maximum number of players: <span className="text-red-500">*</span>
             </label>
             <Input
-              type='number'
-              name='maxplayers'
+              type="number"
+              name="maxplayers"
               required
-              pattern='[0-9]*'
+              pattern="[0-9]*"
+              min={2}
               error={!!formState.errors.playerMax}
             ></Input>
-            <p className='bg-red-500 text-white'>
+            <p className="bg-red-500 text-white">
               {formState.errors.playerMax && formState.errors.playerMax[0]}
             </p>
           </div>
 
-          <div className='flex flex-col mb-3'>
-            <label htmlFor='category'>
+          <div className="flex flex-col mb-3">
+            <label htmlFor="category">
               Please select a suiting category for your game:
             </label>
           </div>
           <div>
             <FormControl sx={{ m: 1, minWidth: 200 }}>
-              <InputLabel id='category'>Select a category:</InputLabel>
+              <InputLabel id="category">Select a category:</InputLabel>
               <Select
-                labelId='category'
-                id='category'
+                labelId="category"
+                id="category"
                 value={selectedCategory}
-                label='Category'
-                name='category'
-                onChange={() => handleCategoryChange}
+                label="Category"
+                name="category"
+                onChange={handleCategoryChange}
               >
-                <MenuItem value=''>
+                <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                <MenuItem value='CARD'>Card</MenuItem>
-                <MenuItem value='DICE'>Dice</MenuItem>
-                <MenuItem value='PHONE'>Phone</MenuItem>
-                <MenuItem value='OTHER'>Other</MenuItem>
+                <MenuItem value="CARD">Card</MenuItem>
+                <MenuItem value="DICE">Dice</MenuItem>
+                <MenuItem value="PHONE">Phone</MenuItem>
+                <MenuItem value="OTHER">Other</MenuItem>
               </Select>
             </FormControl>
           </div>
 
-          <div className='mt-3'>
-            <Button type='submit' variant='contained' className='mb-5'>
+          <div className="mt-3">
+            <Button type="submit" variant="contained" className="mb-5">
               Create game
             </Button>
           </div>
