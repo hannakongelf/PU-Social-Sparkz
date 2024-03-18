1d02b650d57a4abfade67c3389c35c5ac973fa86
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-03-18 12:35:25 +0100

    Add working my profile

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -1,17 +1,27 @@
-'use server';
-import * as React from 'react';
-import { GameWithReviews, getAllGames, getGameById, getReviewsByGame } from '@/db/queries';
-import UserInfoBox from '@/components/my-profile/user-info-box';
+"use server";
 
+import * as React from "react";
+import {
+  GameWithReviews,
+  getAllGames,
+  getGameById,
+  getReviewsByGame,
+} from "@/db/queries";
+import ListContent from "@/components/list-content";
+import UserInfoBox from "@/components/my-profile/user-info-box";
+import { useSession } from "next-auth/react";
+import { Game, gameType } from "@prisma/client";
+import { getGamesByAuthor } from "@/db/queries/game";
+import ListMyGames from "@/components/my-profile/list-my-games";
+import { auth } from "@/auth";
 
-export default async function Home() {
+export default async function Page() {
+  const session = await auth();
+  const games: Game[] = await getGamesByAuthor(session?.user?.id ?? "1");
+
   return (
-
-    <main className='flex justify-center items-center'>
-
-        <UserInfoBox/>
-        {/* <ListContent games={games} /> */}
-        
+    <main className="flex justify-center items-center">
+      <UserInfoBox games={games} user={session?.user} />
     </main>
   );
 }
