9d9c1cdebee2fe08ad5cdc5961873e2a1c9dacdf
irilstr <irils@stud.ntnu.no>
2024-03-18 20:03:08 +0100

    feature: litt fiks

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,3 +7,10 @@ export { addFavoriteGame } from "@/actions/add-favorite-game";
 export { removeFavoriteGame } from "@/actions/remove-favorite-game";
 export { createPersonalList } from "@/actions/create-personal-list";
 export { addToPersonalList } from "@/actions/add-to-personal-list";
+
+export function goToYourFavoriteList(
+  goToYourFavoriteList: any,
+  arg1: { errors: {} }
+): [any, any] {
+  throw new Error("Function not implemented.");
+}
