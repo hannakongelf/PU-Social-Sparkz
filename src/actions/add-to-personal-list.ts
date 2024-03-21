b9a8933214b519e989a13d76b07c920df47a52cc
simonbjerkas <simon.bjerkas10@gmail.com>
2024-03-21 10:49:13 +0100

    redirect on add to list

--- src/actions/add-to-personal-list.ts
+++ src/actions/add-to-personal-list.ts
@@ -4,13 +4,15 @@ import { auth } from '@/auth';
 import { db } from '@/db';
 import * as paths from '@/paths';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function addToPersonalList(gameId: number, queueId: string) {
   const session = await auth();
   if (!session || !session.user)
     throw new Error('You must be logged in to add game to a list.');
+  let queue;
   try {
-    await db.queue.update({
+    queue = await db.queue.update({
       where: {
         id: queueId,
       },
@@ -26,4 +28,5 @@ export async function addToPersonalList(gameId: number, queueId: string) {
 
   revalidatePath(paths.gamePath(gameId));
   revalidatePath(paths.personalList());
+  redirect(paths.personalList(queue?.id));
 }
